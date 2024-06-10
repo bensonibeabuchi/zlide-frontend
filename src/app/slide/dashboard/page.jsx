75360ca91b2aaad3881a723f0c0e906daac4af2d
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-06-10 10:39:06 +0100

    change button UI

--- src/app/slide/dashboard/page.jsx
+++ src/app/slide/dashboard/page.jsx
@@ -148,9 +148,9 @@ export default function Dashboard() {
 
                {/* Pagination controls */}
                <div className='p-4 flex text-center items-center justify-center gap-8 mb-8'>
-                    <button onClick={handlePrevPage} disabled={currentPage === 1} className='text-[#FFD045] bg-gradient-to-r from-[#1F1053] via-[#0A1F79] to-[#5D05C8] px-4 py-2 rounded ' >Previous</button>
+                    <button onClick={handlePrevPage} disabled={currentPage === 1} className='bg-[#FFD045] text-[#0A1F79] px-4 py-2 rounded ' >Previous</button>
                     <span>Page {currentPage} of {totalPages}</span>
-                    <button onClick={handleNextPage} disabled={currentPage === totalPages} className='text-[#FFD045] bg-gradient-to-r from-[#1F1053] via-[#0A1F79] to-[#5D05C8] px-4 py-2 rounded' >Next</button>
+                    <button onClick={handleNextPage} disabled={currentPage === totalPages} className='bg-[#FFD045] text-[#0A1F79] px-4 py-2 rounded' >Next</button>
                 </div>
                 <DeleteModal
                   isOpen={isDeleteModalOpen}
